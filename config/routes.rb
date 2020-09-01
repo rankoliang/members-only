12100318fb0e9519826f34d7422aa57b0df7f181
Ranko Liang <rankoliang@gmail.com>
2020-09-01 16:09:19 -0400

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
