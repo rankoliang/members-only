6ac59c35567c31e780b8a594d0d4760cb5e8a462
Ranko Liang <rankoliang@gmail.com>
2020-09-03 13:17:17 -0400

    Add posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :authenticate, only: %w[new create]
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
